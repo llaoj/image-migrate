d0fd4e700df579aebf2ca3a28c1301f39fdb3e2f
老J <hmmmbiubiubiu@gmail.com>
2024-06-17 17:04:39 +0800

    CI

--- migrate.sh
+++ migrate.sh
@@ -6,7 +6,7 @@ ACR_REPO=registry.cn-beijing.aliyuncs.com
 ACR_IMAGE_REPO=${ACR_REPO}/llaoj
 
 echo "Copy images to Aliyun Container Registry"
-
+cat ./images.txt
 cat ./images.txt | while read line 
 do
     image=${ACR_IMAGE_REPO}/${line/\//_}

0f594d9df349721cf793baa547953aa53587e3e5
老J <hmmmbiubiubiu@gmail.com>
2024-06-17 17:14:59 +0800

    CI

--- migrate.sh
+++ migrate.sh
@@ -7,9 +7,9 @@ ACR_IMAGE_REPO=${ACR_REPO}/llaoj
 
 echo "Copy images to Aliyun Container Registry"
 
-cat ./images.txt | while read line 
+while read line 
 do
-    echo "Copying $line"
-    image=${ACR_IMAGE_REPO}/${line/\//_}
-    skopeo copy docker://"${line}" docker://"${image}"
-done
+  echo "Copying $line"
+  image=${ACR_IMAGE_REPO}/${line/\//_}
+  skopeo copy docker://"${line}" docker://"${image}"
+done < "./images.txt"

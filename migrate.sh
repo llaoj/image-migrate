79f45ed64e501c293f599e0508020da8aebd56af
老J <hmmmbiubiubiu@gmail.com>
2024-06-17 19:25:49 +0800

    CI

--- migrate.sh
+++ migrate.sh
@@ -5,10 +5,10 @@ set -x
 ACR_REPO=registry.cn-beijing.aliyuncs.com
 ACR_IMAGE_REPO=${ACR_REPO}/llaoj
 
-cat images.txt | while read line
+while IFS= read -r line
 do
   echo "Copying $line ..."
   image=${line#*/}
   image=${ACR_IMAGE_REPO}/${image/\//_}
   skopeo copy docker://"${line}" docker://"${image}"
-done
+done < images.txt

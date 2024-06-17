34317909be22207cf39607c4c49538196c1551b4
老J <hmmmbiubiubiu@gmail.com>
2024-06-17 17:09:35 +0800

    CI

--- migrate.sh
+++ migrate.sh
@@ -6,9 +6,10 @@ ACR_REPO=registry.cn-beijing.aliyuncs.com
 ACR_IMAGE_REPO=${ACR_REPO}/llaoj
 
 echo "Copy images to Aliyun Container Registry"
-cat ./images.txt
+
 cat ./images.txt | while read line 
 do
+    echo "Copying $line"
     image=${ACR_IMAGE_REPO}/${line/\//_}
     skopeo copy docker://"${line}" docker://"${image}"
 done

cbfb71e64d82aab2eb21c497853235a4639be024
老J <hmmmbiubiubiu@gmail.com>
2024-06-17 19:55:52 +0800

    CI

--- migrate.sh
+++ migrate.sh
@@ -2,13 +2,9 @@
 
 set -x
 
-ACR_REPO=registry.cn-beijing.aliyuncs.com
-ACR_IMAGE_REPO=${ACR_REPO}/llaoj
-
-while IFS= read -r line
-do
-  echo "Copying $line ..."
-  image=${line#*/}
-  image=${ACR_IMAGE_REPO}/${image/\//_}
-  skopeo copy docker://"${line}" docker://"${image}"
-done < images.txt
+cat images.txt | awk '{ 
+  dst = $1;
+  gsub(/^[^\/]+\//, "", dst);
+  gsub("/", "_", dst);
+  print "skopeo copy docker://"$1" docker://registry.cn-beijing.aliyuncs.com/llaoj/"dst;
+}' | xargs -I {} sh -c "{}"

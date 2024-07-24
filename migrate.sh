6f0f8e3a00ff2f925018cf4f9000ef73d5b9ed71
老J <hmmmbiubiubiu@gmail.com>
2024-07-24 17:34:31 +0800

    skopeo

--- migrate.sh
+++ migrate.sh
@@ -6,5 +6,5 @@ cat images.txt | awk '{
   dst = $1;
   gsub(/^[^\/]+\//, "", dst);
   gsub("/", "_", dst);
-  print "docker run -t quay.io/skopeo/stable:latest copy docker://"$1" docker://registry.cn-beijing.aliyuncs.com/llaoj/"dst;
+  print "skopeo copy docker://"$1" docker://registry.cn-beijing.aliyuncs.com/llaoj/"dst;
 }' | xargs -I {} sh -c "{}"

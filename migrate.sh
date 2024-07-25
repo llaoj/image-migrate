fcd2075c59d60c87672f9d274fb819a35ee661cf
老J <hmmmbiubiubiu@gmail.com>
2024-07-25 19:57:19 +0800

    multi arch

--- migrate.sh
+++ migrate.sh
@@ -3,8 +3,12 @@
 set -x
 
 tail /tmp/images.txt | awk '{ 
-  dst = $1;
-  gsub(/^[^\/]+\//, "", dst);
-  gsub("/", "_", dst);
+  if(NF==2){
+    dst = $2;
+  }else{
+    dst = $1;
+    gsub(/^[^\/]+\//, "", dst);
+    gsub("/", "_", dst);
+  }
   print "skopeo copy --multi-arch all docker://"$1" docker://registry.cn-beijing.aliyuncs.com/llaoj/"dst;
 }' | xargs -I {} sh -c "{}"
